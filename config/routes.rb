de47968c3ff8bcabf844e79a49047bd250481270
Terasawa Kazuyuki <ie2965@hotmail.com>
2019-03-23 17:20:27 +0900

    Edit show action to routs

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'tweets'     => 'tweets#index'
   get 'tweets/new' => 'tweets#new'
   post 'tweets'    => 'tweets#create'
+  get 'tweets/:id' => 'tweets#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
