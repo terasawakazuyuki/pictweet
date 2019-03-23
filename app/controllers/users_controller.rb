a525b32184f264cd7f47a85d7b1094a672eb3488
Terasawa Kazuyuki <ie2965@hotmail.com>
2019-03-23 19:09:46 +0900

    Edit users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  class UsersController < ApplicationController
+    def show
+      @nickname = current_user.nickname
+      @tweets = Tewwt.where(user_id: current_user.id).page(params[:page]).per(5).order("created_at DESC")
+    end
+  end
 end
