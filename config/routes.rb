3ac4bd8284f6672f0f353d9c03f9b7545a64db2a
Terasawa Kazuyuki <ie2965@hotmail.com>
2019-03-23 15:18:50 +0900

    add pageant to tweet_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'tweets#index'
-  get '/tweets'     => 'tweets#index'
+  get 'tweets'     => 'tweets#index'
   get 'tweets/new' => 'tweets#new'
   post 'tweets'    => 'tweets#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
