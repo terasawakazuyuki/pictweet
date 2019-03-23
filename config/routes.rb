17ba4af287566fd0f6475affaba4891534d1ce81
Terasawa Kazuyuki <ie2965@hotmail.com>
2019-03-23 19:29:33 +0900

    Edit users_controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'tweets'     => 'tweets#index'
   get 'tweets/new' => 'tweets#new'
   post 'tweets'    => 'tweets#create'
-  get 'tweets/:id' => 'tweets#show'
+  get 'users/:id' => 'users#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
